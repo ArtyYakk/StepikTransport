ba191f2d2a051c577da7c934953ab32bc26b3e12
iakon <iakonien@gmail.com>
2022-06-09 21:39:59 +0300

    Добавил фичи

--- src/trans/steam/Steam.java
+++ src/trans/steam/Steam.java
@@ -3,4 +3,7 @@ package trans.steam;
 import trans.Transport;
 
 public interface Steam extends Transport {
+    default void beep(){
+        System.out.println("Beep Beeep");
+    }
 }
--- src/trans/war/War.java
+++ src/trans/war/War.java
@@ -3,4 +3,7 @@ package trans.war;
 import trans.Transport;
 
 public interface War extends Transport {
+    default void battle(){
+        System.out.println("prepear for the battle");
+    }
 }
